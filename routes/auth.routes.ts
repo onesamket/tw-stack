2053c8130d510fe1f431368a68a4452f7ab53e46
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-06-06 01:28:48 +0300

    remove unused staffs

--- routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import authController from "@server/controllers/auth.controller";
-import { Router } from "express";
-
-const authRouter = Router();
-
-authRouter.post("/login", authController.login);
-
-export default authRouter;
