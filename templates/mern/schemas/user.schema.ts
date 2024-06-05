30523442e32cc2e60b2dd8884439f6525dbc0881
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-06-06 01:19:27 +0300

    Create docs site  using nextjs and shadcnUI

--- templates/mern/schemas/user.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-// Define schema for user creation
-export const UserSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(6),
-});
