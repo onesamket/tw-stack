0e9c738968a7f49e2bb83ee46f6b8eff7a0afd7b
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-06-10 11:37:52 +0300

    add  contributors

---
+++ constants/contributors.ts
@@ -0,0 +1,34 @@
+export const CONTRIBUTORS = [
+  {
+    name: "Tewodros Birhanu",
+    githubLink: "https://github.com/onesamket",
+    imageSrc: "https://github.com/onesamket.png",
+    twitterLink: "https://x.com/onesamket",
+    twitterHandle: "onesamket",
+    devType: "Javascript Full-stack Developer",
+  },
+  {
+    name: "Daniel Masfin ",
+    githubLink: "https://github.com/DanielMesfin",
+    imageSrc: "https://github.com/DanielMesfin.png",
+    twitterLink: "https://x.com/DanielMesfin",
+    twitterHandle: "DanielMesfin",
+    devType: "Spring  & Web3 Developer",
+  },
+  {
+    name: "Asmare  Admasu",
+    githubLink: "https://github.com/asm2212",
+    imageSrc: "https://github.com/asm2212.png",
+    twitterLink: "https://x.com/asm2212",
+    twitterHandle: "asm2212",
+    devType: "Web  Developer",
+  },
+  {
+    name: "Natneal Sitotaw",
+    githubLink: "https://github.com/natisitotaw",
+    imageSrc: "https://github.com/natisitotaw.png",
+    twitterLink: "https://x.com/natisitotaw",
+    twitterHandle: "natisitotaw",
+    devType: "Game and Backend  Developer",
+  },
+];
