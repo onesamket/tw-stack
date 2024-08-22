5ab1c62977808c6d8ae0abfceec2632d7221cc43
Tewodros Birhan <120477045+onesamket@users.noreply.github.com>
2024-08-23 00:39:59 +0300

    Update contributors.ts

--- constants/contributors.ts
+++ constants/contributors.ts
@@ -1,6 +1,6 @@
 export const CONTRIBUTORS = [
   {
-    name: "Tewodros Birhanu",
+    name: "Tewodros Birhan",
     githubLink: "https://github.com/onesamket",
     imageSrc: "https://github.com/onesamket.png",
     twitterLink: "https://x.com/onesamket",
@@ -15,22 +15,7 @@ export const CONTRIBUTORS = [
     twitterHandle: "DanielMesfin",
     devType: "Spring  & Web3 Developer",
   },
-  {
-    name: "Asmare  Admasu",
-    githubLink: "https://github.com/asm2212",
-    imageSrc: "https://github.com/asm2212.png",
-    twitterLink: "https://x.com/asm2212",
-    twitterHandle: "asm2212",
-    devType: "Fullstack Engineer | Mobile Developer",
-  },
-  {
-    name: "Natneal Sitotaw",
-    githubLink: "https://github.com/natisitotaw",
-    imageSrc: "https://github.com/natisitotaw.png",
-    twitterLink: "https://x.com/natisitotaw",
-    twitterHandle: "natisitotaw",
-    devType: "Game and Backend  Developer",
-  },
+ 
   {
     name: "Amanuel Kassahun",
     githubLink: "https://github.com/amankass",
