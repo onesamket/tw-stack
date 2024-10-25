014690a9ff8d54db48df505311df20a5a4e35657
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-10-25 22:09:57 +0300

    Update contributors.ts

--- constants/contributors.ts
+++ constants/contributors.ts
@@ -1,11 +1,11 @@
 export const CONTRIBUTORS = [
   {
-    name: "Tewodros Birhan",
+    name: "Tewodros Birhanu",
     githubLink: "https://github.com/onesamket",
     imageSrc: "https://github.com/onesamket.png",
     twitterLink: "https://x.com/onesamket",
     twitterHandle: "onesamket",
-    devType: "Javascript Full-stack & App Developer",
+    devType: "Software Engineer",
   },
   {
     name: "Daniel Masfin ",
@@ -13,7 +13,7 @@ export const CONTRIBUTORS = [
     imageSrc: "https://github.com/DanielMesfin.png",
     twitterLink: "https://x.com/DanielMesfin",
     twitterHandle: "DanielMesfin",
-    devType: "Spring  & Web3 Developer",
+    devType: "Spring, Javascript Fullstack  & Web3 Developer",
   },
  
   {
